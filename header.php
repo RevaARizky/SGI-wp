a734d014e44f3e04478a074f0f495cec83ed441f
RevaARizky <revaathallah86@gmail.com>
2024-03-29 00:07:42 +0800

    header update contact

--- header.php
+++ header.php
@@ -61,17 +61,17 @@
                     <div class="col-span-1">
                         <div class="menu-wrapper flex gap-x-14 justify-end">
                             <a href="/about-us">
-                                <span class="text-white md:text-xl text-xl font-montserrat ">
+                                <span class="text-white md:text-lg text-xl font-montserrat ">
                                     About SGI
                                 </span>
                             </a>
                             <a href="/our-fleet">
-                                <span class="text-white md:text-xl text-xl font-montserrat ">
+                                <span class="text-white md:text-lg text-xl font-montserrat ">
                                     Our Fleet
                                 </span>
                             </a>
                             <a href="/gallery">
-                                <span class="text-white md:text-xl text-xl font-montserrat ">
+                                <span class="text-white md:text-lg text-xl font-montserrat ">
                                     Gallery
                                 </span>
                             </a>
@@ -94,23 +94,32 @@
                     </div>
 
                     <div class="col-span-1">
-                        <div class="menu-wrapper flex gap-x-14 justify-start relative">
+                        <div class="menu-wrapper flex gap-x-14 justify-start relative items-center">
                             <a href="/blog">
-                                <span class="text-white md:text-xl text-xl font-montserrat ">
+                                <span class="text-white md:text-lg text-xl font-montserrat ">
                                     Blog
                                 </span>
                             </a>
-                            <a href="/contact">
-                                <span class="text-white md:text-xl text-xl font-montserrat ">
-                                    Contact
+                            <a href="/contact" class="md:px-8 px-4 md:py-4 py-2 bg-sgi-orange md:text-xs text-[10px] text-white">
+                                <span class="">
+                                    Contact Us
                                 </span>
                             </a>
-                            <div class="social-media-wrapper flex gap-x-8 absolute right-0 top-1/2 -translate-y-1/2">
+                            <div class="social-media-wrapper flex gap-x-4 absolute right-0 top-1/2 -translate-y-1/2">
                                 <div class="social">
-                                    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-[1.25rem] h-full" fill="#fff"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg></a>
+                                    <a href="#">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="#fff" class="w-[1.125rem] h-[1.125rem]"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"/></svg>
+                                    </a>
                                 </div>
                                 <div class="social">
-                                    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-[1.25rem] h-full" fill="#fff"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/></svg></a>
+                                    <a href="#">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" fill="#fff" class="w-[1.125rem] h-[1.125rem]"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"/></svg>
+                                    </a>
+                                </div>
+                                <div class="social">
+                                    <a href="#">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="#fff" class="w-[1.125rem] h-[1.125rem]"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+                                    </a>
                                 </div>
                             </div>
                         </div>
