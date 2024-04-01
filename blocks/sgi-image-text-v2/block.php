25f14bb4666f3c23cf8795162dba552d6c4b6d81
RevaARizky <revaathallah86@gmail.com>
2024-04-01 17:03:37 +0800

    add relative

--- blocks/sgi-image-text-v2/block.php
+++ blocks/sgi-image-text-v2/block.php
@@ -16,7 +16,7 @@ if ( ! empty($block['anchor'] ) ) {
     $id = $block['anchor'];
 }
 // Create class attribute allowing for custom "className" and "align" values.
-$classes = 'acf-block block-image-text text-white';
+$classes = 'acf-block block-image-text text-white relative';
 if ( ! empty( $block['className'] ) ) {
     $classes .= ' ' . $block['className'];
 }
